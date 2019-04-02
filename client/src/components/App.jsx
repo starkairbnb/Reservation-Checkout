95afb1bf00f5717171fb7bbb570da561ad8de30c
charles-spinosa <charles.spinosa@gmail.com>
2019-04-02 15:50:56 -0700

    [ prog ] begin implementing Airbnb react-date

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,7 +3,15 @@ import PricePerNight from './PricePerNight';
 import Dates from './Dates';
 import Guests from './Guests';
 
-import Calendar from 'react-calendar/dist/entry.nostyle';
+// import Calendar from 'react-calendar/dist/entry.nostyle';
+
+import 'react-dates/initialize';
+import {
+  DateRangePicker,
+  SingleDatePicker,
+  DayPickerRangeController
+} from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -24,7 +32,20 @@ class App extends React.Component {
         <div>
           <Guests />
         </div>
-        <Calendar />
+        <div>
+          <DateRangePicker
+            startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+            startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+            endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+            endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+            onDatesChange={({ startDate, endDate }) => {
+              console.log(startDate, endDate);
+              this.setState({ startDate, endDate });
+            }} // PropTypes.func.isRequired,
+            focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+            onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+          />
+        </div>
       </div>
     );
   }
