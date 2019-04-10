f5c144f79a2da900a1c20394e27b4830aef533e2
charles-spinosa <charles.spinosa@gmail.com>
2019-04-10 13:09:35 -0700

    [ feat ] finished tooltips

--- client/src/components/Summary.jsx
+++ client/src/components/Summary.jsx
@@ -49,6 +49,20 @@ class Summary extends React.Component {
         />
       </svg>
     );
+    let icon3 = (
+      <svg
+        className="icon3"
+        viewBox="0 0 24 24"
+        role="img"
+        aria-label="Close"
+        focusable="false"
+      >
+        <path
+          d="m23.25 24c-.19 0-.38-.07-.53-.22l-10.72-10.72-10.72 10.72c-.29.29-.77.29-1.06 0s-.29-.77 0-1.06l10.72-10.72-10.72-10.72c-.29-.29-.29-.77 0-1.06s.77-.29 1.06 0l10.72 10.72 10.72-10.72c.29-.29.77-.29 1.06 0s .29.77 0 1.06l-10.72 10.72 10.72 10.72c.29.29.29.77 0 1.06-.15.15-.34.22-.53.22"
+          fillRule="evenodd"
+        />
+      </svg>
+    );
 
     return (
       <div className="summary-container">
@@ -75,6 +89,9 @@ class Summary extends React.Component {
                   <span className="tooltiptext cleaning-tip">
                     One-time fee charged by host to cover the cost of cleaning
                     their space.
+                    <div className="x-button" tabIndex="-1">
+                      {icon3}
+                    </div>
                   </span>
                 </button>
               </div>
@@ -93,6 +110,9 @@ class Summary extends React.Component {
                   <span className="tooltiptext service-tip">
                     This helps us run our platform and offer services like 24/7
                     support on your trip.
+                    <div className="x-button" tabIndex="-1">
+                      {icon3}
+                    </div>
                   </span>
                 </button>
               </div>
@@ -111,6 +131,9 @@ class Summary extends React.Component {
                   <span className="tooltiptext tax-tip">
                     This is a municipal accomodations tax necessary for
                     ShareBnB.
+                    <div className="x-button" tabIndex="-1">
+                      {icon3}
+                    </div>
                   </span>
                 </button>
               </div>
@@ -142,6 +165,9 @@ class Summary extends React.Component {
               <span className="tooltiptext due-tip">
                 If you prefer to pay the full reservation amount now, you can
                 choose this option in Confirm and pay.
+                <div className="x-button" tabIndex="-1">
+                  {icon3}
+                </div>
               </span>
             </button>
           </div>
