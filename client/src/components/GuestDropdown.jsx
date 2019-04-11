5f48a58461d0f441708aebb1567d801a4f3e2cba
charles-spinosa <charles.spinosa@gmail.com>
2019-04-10 18:17:27 -0700

    [ prog ] popover rendering appropriately, no functionality

--- client/src/components/GuestDropdown.jsx
+++ client/src/components/GuestDropdown.jsx
@@ -10,12 +10,26 @@ class GuestDropdown extends React.Component {
     this.state = {
       adults: 1,
       children: 0,
-      infants: 0,
+      infants: 1,
       open: false
     };
   }
 
   render() {
+    let carrot = (
+      <svg
+        className={`carrot${this.state.open ? ' open_guests' : ''}`}
+        viewBox="0 0 18 18"
+        role="presentation"
+        aria-hidden="true"
+        focusable="false"
+      >
+        <path
+          d="m1.71 13.71a1 1 0 1 1 -1.42-1.42l8-8a1 1 0 0 1 1.41 0l8 8a1 1 0 1 1 -1.41 1.42l-7.29-7.29z"
+          fillRule="evenodd"
+        />
+      </svg>
+    );
     return (
       <OutsideClickHandler
         onOutsideClick={() => {
@@ -27,17 +41,34 @@ class GuestDropdown extends React.Component {
             className="guests-button"
             onClick={() => this.setState({ open: !this.state.open })}
           >
-            2 Guests
+            <div className="guests-button-flex">
+              <div className="guests-button-text">
+                {this.state.adults + this.state.children} Guests
+                {this.state.infants > 0
+                  ? `, ${this.state.infants} infants`
+                  : ''}
+              </div>
+              {carrot}
+            </div>
           </button>
           {this.state.open && (
             <div className="guests-buttons-container">
-              <div>
-                Hi;alkskdjf;laskdjf;al kdjf;laksdjf;laksjdf; laksjdf;laks
-                jf;laksdjf;alksdjf;alskfja;sldkjfa;slkdfja;slkdjf;klj
+              <div className="guests-component-container">
+                <Guests />
+              </div>
+              <div className="max-guests">
+                {`${
+                  this.props.maxGuests
+                } guests maximum. Infants don’t count toward the number of guests.`}
+              </div>
+              <div
+                className="close"
+                onClick={() => this.setState({ open: false })}
+              >
+                Close
               </div>
             </div>
           )}
-          <div>This is a sibling to my conditional stuff</div>
         </div>
       </OutsideClickHandler>
     );
