f31396babf497aaf3a62fd9de03179a63c58f2aa
Uttej Gujral <uttej@Uttejs-MacBook-Pro.local>
2019-04-30 19:27:22 -0700

    Small fix

--- database/mongo/index.js
+++ database/mongo/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Promise = require('bluebird');
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/reservation', {
+mongoose.connect('mongodb://localhost:27017/reservation', {
   useNewUrlParser: true
 });
 
