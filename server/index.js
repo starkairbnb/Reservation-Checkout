59d927d5b2e61b4d756e77ed60d1aa72c9eb47e9
Uttej Gujral <uttej@Uttejs-MacBook-Pro.local>
2019-05-03 09:50:49 -0700

    Commented morgan to test server response time

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3003;
 
 
 app.use(bodyParser.json());
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
